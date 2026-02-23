e4d4195e1697035991e8af8e95713660cc52c8ed
unknown <franciscowan@outlook.com>
2026-02-23 16:10:58 -0300

    versão final UI

--- frontend/src/app/shared/buttons/fa-circle-plus/fa-circle-plus.ts
+++ frontend/src/app/shared/buttons/fa-circle-plus/fa-circle-plus.ts
@@ -11,7 +11,7 @@ import { faCirclePlus, IconDefinition } from '@fortawesome/free-solid-svg-icons'
     (click)="onClick()"
     class="w-6 h-6 flex items-center justify-center 
             bg-white rounded-full shadow-md 
-            hover:scale-105 transition-all duration-200 m-4">
+            hover:scale-105 transition-all duration-200 m-4 cursor-pointer">
         <fa-icon
             [icon]="icon()"
             class="text-blue-700 text-4xl pointer-events-none m-4 hover:scale-105 transition-all duration-200">
